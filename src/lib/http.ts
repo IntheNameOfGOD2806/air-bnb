cb912cd79d7e7102cbe38f9d49b79676dee80981
dattran2806 <tranthanhdat4321@gmail.com>
2025-05-19 14:39:43 +0700

    .env update

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://air-bnb-server.onrender.com/";
 const jwtkey = "accessToken";
 
 axios.interceptors.request.use(
