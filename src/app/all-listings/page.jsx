5bc5d29fbb02fb830ba6bc04480939bf66ee50c7
dattran2806 <tranthanhdat4321@gmail.com>
2025-06-08 18:00:21 +0700

    sad
    ads

--- src/app/all-listings/page.jsx
+++ src/app/all-listings/page.jsx
@@ -1,9 +1,15 @@
-import React from "react";
 
-export const Page = () => {
+import React, { useState } from "react";
+import AppWrapper from "../wrapper";
+const AllListings = () => {
     return (
-        <div>
-            <h1>Page</h1>
-        </div>
-    );
-};
+        <>
+            <AppWrapper>
+                <script src="https://static.elfsight.com/platform/platform.js" async></script>
+                <div class="elfsight-app-a672d481-e953-4ab4-be13-92d85b6b1ad9" data-elfsight-app-lazy></div>
+            </AppWrapper>
+        </>
+    )
+}
+
+export default AllListings
