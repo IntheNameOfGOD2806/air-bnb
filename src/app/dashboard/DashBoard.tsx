e4b77e5657d1c9e2882203f7cc82c6f047b8f089
dattran2806 <tranthanhdat4321@gmail.com>
2025-06-23 10:13:53 +0700

    zxc
    xzcz

--- src/app/dashboard/DashBoard.tsx
+++ src/app/dashboard/DashBoard.tsx
@@ -13,99 +13,167 @@ import { TasksProgress } from "@/components/dashboard/overview/tasks-progress";
 import { TotalCustomers } from "@/components/dashboard/overview/total-customers";
 import { TotalProfit } from "@/components/dashboard/overview/total-profit";
 import { Traffic } from "@/components/dashboard/overview/traffic";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import { UserProvider } from "@/context/user-context";
+import { LocalizationProvider } from "@/components/core/localization-provider";
+import { ThemeProvider } from "@/components/core/theme-provider/theme-provider";
+import { AuthGuard } from "@/components/auth/auth-guard";
+import { MainNav } from "@/components/dashboard/layout/main-nav";
+import { SideNav } from "@/components/dashboard/layout/side-nav";
+import AppWrapper from "../wrapper";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export function Layout({ children }: LayoutProps): React.JSX.Element {
+  return (
+    <AppWrapper>
+      <LocalizationProvider>
+        {/* <UserProvider> */}
+        <ThemeProvider>
+          {/* <AuthGuard> */}
+          <GlobalStyles
+            styles={{
+              body: {
+                "--MainNav-height": "56px",
+                "--MainNav-zIndex": 1000,
+                "--SideNav-width": "280px",
+                "--SideNav-zIndex": 1100,
+                "--MobileNav-width": "320px",
+                "--MobileNav-zIndex": 1100,
+              },
+            }}
+          />
+          <Box
+            sx={{
+              bgcolor: "var(--mui-palette-background-default)",
+              display: "flex",
+              flexDirection: "column",
+              position: "relative",
+              minHeight: "100%",
+            }}
+          >
+            {/* <SideNav /> */}
+            <Box
+              sx={{
+                display: "flex",
+                flex: "1 1 auto",
+                flexDirection: "column",
+                pl: { lg: "" },
+              }}
+            >
+              {/* <MainNav /> */}
+              <main>
+                <Container maxWidth="xl" sx={{ py: "64px" }}>
+                  {children}
+                </Container>
+              </main>
+            </Box>
+          </Box>
+          {/* </AuthGuard> */}
+        </ThemeProvider>
+        {/* </UserProvider> */}
+      </LocalizationProvider>
+    </AppWrapper>
+  );
+}
 
 export default function DashBoard(): React.JSX.Element {
   return (
-    <Grid container spacing={3}>
-      <Grid
-        size={{
-          lg: 3,
-          sm: 6,
-          xs: 12,
-        }}
-      >
-        <Budget diff={12} trend="up" sx={{ height: "100%" }} value="797" />
-      </Grid>
-      <Grid
-        size={{
-          lg: 3,
-          sm: 6,
-          xs: 12,
-        }}
-      >
-        <TotalCustomers
-          diff={16}
-          trend="down"
-          sx={{ height: "100%" }}
-          value="65"
-        />
-      </Grid>
-      <Grid
-        size={{
-          lg: 3,
-          sm: 6,
-          xs: 12,
-        }}
-      >
-        <TasksProgress
-          sx={{ height: "100%" }}
-          value="20"
-          trend="up"
-          diff={16}
-        />
-      </Grid>
-      <Grid
-        size={{
-          lg: 3,
-          sm: 6,
-          xs: 12,
-        }}
-      >
-        <TotalProfit sx={{ height: "100%" }} value="50" />
-      </Grid>
-      <Grid
-        size={{
-          lg: 8,
-          xs: 12,
-        }}
-      >
-        {/* <Sales
+    <Layout>
+      <Grid container spacing={3}>
+        <Grid
+          size={{
+            lg: 3,
+            sm: 6,
+            xs: 12,
+          }}
+        >
+          <Budget diff={12} trend="up" sx={{ height: "100%" }} value="797" />
+        </Grid>
+        <Grid
+          size={{
+            lg: 3,
+            sm: 6,
+            xs: 12,
+          }}
+        >
+          <TotalCustomers
+            diff={16}
+            trend="down"
+            sx={{ height: "100%" }}
+            value="65"
+          />
+        </Grid>
+        <Grid
+          size={{
+            lg: 3,
+            sm: 6,
+            xs: 12,
+          }}
+        >
+          <TasksProgress
+            sx={{ height: "100%" }}
+            value="20"
+            trend="up"
+            diff={16}
+          />
+        </Grid>
+        <Grid
+          size={{
+            lg: 3,
+            sm: 6,
+            xs: 12,
+          }}
+        >
+          <TotalProfit sx={{ height: "100%" }} value="50" />
+        </Grid>
+        <Grid
+          size={{
+            lg: 8,
+            xs: 12,
+          }}
+        >
+          {/* <Sales
           chartSeries={[
             { name: 'This year', data: [18, 16, 5, 8, 3, 14, 14, 16, 17, 19, 18, 20] },
             { name: 'Last year', data: [12, 11, 4, 6, 2, 9, 9, 10, 11, 12, 13, 13] },
           ]}
           sx={{ height: '100%' }}
         /> */}
-        <BookingsThisWeek
-          chartSeries={[
-            {
-              name: "Bookings",
-              data: [0, 0, 0, 0, 0, 0, 0], // Thứ 2 -> Chủ nhật
-            },
-          ]}
-        />
-      </Grid>
-      <Grid
-        size={{
-          lg: 4,
-          md: 6,
-          xs: 12,
-        }}
-      >
-        <Traffic
-          chartSeries={[502, 103, 175]}
-          labels={["Desktop", "Tablet", "Phone"]}
-          sx={{ height: "100%" }}
-        />
-      </Grid>
-      <Grid
-        size={{
-          lg: 4,
-          md: 6,
-          xs: 12,
-        }}
-      >
-        {/* <LatestProducts
+          <BookingsThisWeek
+            chartSeries={[
+              {
+                name: "Bookings",
+                data: [0, 0, 0, 0, 0, 0, 0], // Thứ 2 -> Chủ nhật
+              },
+            ]}
+          />
+        </Grid>
+        <Grid
+          size={{
+            lg: 4,
+            md: 6,
+            xs: 12,
+          }}
+        >
+          <Traffic
+            chartSeries={[502, 103, 175]}
+            labels={["Desktop", "Tablet", "Phone"]}
+            sx={{ height: "100%" }}
+          />
+        </Grid>
+        <Grid
+          size={{
+            lg: 4,
+            md: 6,
+            xs: 12,
+          }}
+        >
+          {/* <LatestProducts
           products={[
             {
               id: "PRD-005",
@@ -207,7 +275,8 @@ export default function DashBoard(): React.JSX.Element {
           ]}
           sx={{ height: "100%" }}
         /> */}
+        </Grid>
       </Grid>
-    </Grid>
+    </Layout>
   );
 }
